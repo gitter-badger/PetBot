b5c752082ad12f7e3b9df98601c4e3cf1516c13e
Petri Autio <petri.mikael.autio@gmail.com>
2016-12-31 22:29:13 +0000

    Fixing settings.py

--- PetBot/settings.py
+++ PetBot/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'chatterbot.ext.django_chatterbot',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -48,7 +49,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-	'chatterbot.ext.django_chatterbot',
+
 ]
 
 ROOT_URLCONF = 'petbot.urls'
