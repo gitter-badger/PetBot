61ca834e9c942a11da471168a00dc3ca11fdd83c
Petri Autio <petri.mikael.autio@gmail.com>
2016-12-31 22:08:13 +0000

    Creating Django service for bot

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Petbot',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Petri',
+    author_email='',
+    description=''
+)
