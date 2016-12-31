509ed5396b208b398616a3d740b20dc402b8f968
Petri Autio <petri.mikael.autio@gmail.com>
2016-12-31 01:25:13 +0000

    Input and Output online with Gitter

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
+import logging
+
+'''
+See the HipChat api documentation for how to get a user access token.
+https://developer.atlassian.com/hipchat/guide/hipchat-rest-api/api-access-tokens
+'''
+
+# gitter api token 291b8aa0b216cbf5d6429b215f979ada5e0a4ba9
 
 chatbot = ChatBot(
     "PetBot",
     storage_adapter="chatterbot.storage.JsonFileStorageAdapter",
-    input_adapter="chatterbot.input.TerminalAdapter",
-    output_adapter="chatterbot.output.TerminalAdapter",
+    #_host="https://archbang.hipchat.com",
+    gitter_room="autio/main",
+    gitter_api_token="291b8aa0b216cbf5d6429b215f979ada5e0a4ba9",
+    gitter_only_respond_to_mentions=False,
+    input_adapter="chatterbot.input.Gitter", #TerminalAdapter",
+    output_adapter="chatterbot.output.Gitter",
     database="./database.json"
 )
 
@@ -14,6 +26,8 @@ conversation = [
     "Hi there!",
     "How are you doing?",
     "I'm doing great.",
+    "What's up?",
+    "Things are swell.",
     "That is good to hear",
     "Thank you.",
     "You're welcome."
